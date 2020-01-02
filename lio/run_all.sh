5d69b3949adc93704200e6ae64b42500ba33bc57
Luiz Felix <lzcfelix@gmail.com>
2020-01-02 11:15:48 -0300

    Updating runner script

--- lio/run_all.sh
+++ lio/run_all.sh
@@ -4,7 +4,7 @@ for fn in 'sphere' 'csendes' 'salomon' 'ackley1' 'alpine1' 'rastrigin' 'schwefel
 do
     for vars in 10 25 50 100
     do
-        python run_experiment.py $fn -n_vars $vars -result_dest ./results_4/$fn/$vars
+        python run_experiment.py $fn -n_vars $vars -result_dest ./results/$fn/$vars
     done
 done
 
